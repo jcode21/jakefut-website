6915dacdec3da8eed8e5719384f5808170e401fd
James Carrillo <jamescarrillo@MacBook-Pro-de-James.local>
2025-03-25 20:08:23 -0500

    update host

--- util.js
+++ util.js
@@ -1,2 +1,2 @@
-const HOST = 'https://api-jakefutbol.redpos.app/matches'
+const HOST = 'https://api-jakefutbol.redpos.app/app-futbol/generic'
 //const HOST = "http://localhost:8080/app-futbol/generic"; 
